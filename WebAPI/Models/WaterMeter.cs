622611b5a67b05bb720c4c6a4a8dab01675f4014
Dmitry Panov <dmitry.panov@danfoss.com>
2019-07-10 14:03:12 +0300

    Create Models

---
+++ WebAPI/Models/WaterMeter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class WaterMeter
+    {
+        private int Id {get; }
+        private int HomeId { get; set; }
+        private string FactoryNumber { get; set; }
+        private int MeterReading { get; set; }
+        private int CurrId = 0;
+
+        public WaterMeter()
+        {
+            Id = CurrId++;
+        }
+    }
+}
